27bde8a0e4ed223f6404729eff0260306c3886dd
George Taray <gtaray@hotmail.com>
2023-05-08 09:04:12 -0700

    Moved the logic to update fields when damage type game option changes to templates

--- common/scripts/string_damagetype_autocomplete.lua
+++ common/scripts/string_damagetype_autocomplete.lua
@@ -1,3 +1,21 @@
+function onInit()
+	OptionsManager.registerCallback("DMGTYPES", updateDamageTypeOption);
+	updateDamageTypeOption();
+end
+function onClose()
+	OptionsManager.unregisterCallback("DMGTYPES", updateDamageTypeOption);
+end
+
+function updateDamageTypeOption()
+	local bDmgTypes = OptionsManagerCypher.replaceArmorWithDamageTypes();
+	self.setVisible(bDmgTypes)
+
+	local sLabelName = getName() .. "_label";
+	if window[sLabelName] then
+		window[sLabelName].setVisible(bDmgTypes)
+	end
+end
+
 function onChar()
 	if super and super.onChar then
 		super.onChar();
