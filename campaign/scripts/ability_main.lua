6e450c4639bf0ee02f09472d56c7957f3529422a
George Taray <gtaray@hotmail.com>
2023-06-05 11:22:04 -0700

    Moved record_ability script to a lua file. Used callSafeControlUpdate when possible, but that function doesn't work on stringcyclers. So can't really use it everywhere.

---
+++ campaign/scripts/ability_main.lua
@@ -0,0 +1,44 @@
+function onInit()
+	update();
+end
+
+function update()
+	local bReadOnly = WindowManager.getReadOnlyState(getDatabaseNode());
+	local bHasCost = cost.getValue() ~= 0 and coststat.getValue() ~= "-";
+	local bShowCost = bHasCost or not bReadOnly;
+	local bHasType = type.getValue() ~= "";
+	local bHasRecharge = period.getValue() ~= "-";
+	local bHasUseType = usetype.getValue() ~= "-";
+
+	WindowManager.callSafeControlUpdate(self, "type", bReadOnly, not bHasType and bReadOnly)
+
+	-- Can't use callSafeControlUpdate on these fields because
+	-- that function doesn't work on string cyclers
+	usetype.setReadOnly(bReadOnly);
+	usetype.setVisible(bHasUseType or not bReadOnly);
+	usetype_label.setReadOnly(bReadOnly);
+	usetype_label.setVisible(bHasUseType or not bReadOnly);
+
+	useequipped.setReadOnly(bReadOnly);
+	useequipped.setVisible(bHasType or not bReadonly);
+	useequipped_label.setReadOnly(bReadOnly);
+	useequipped_label.setVisible(bHasType or not bReadonly);
+
+	period.setReadOnly(bReadOnly);
+	period.setVisible(bHasRecharge or not bReadOnly);
+	period_label.setReadOnly(bReadOnly);
+	period_label.setVisible(bHasRecharge or not bReadOnly);
+
+	header_cost.setVisible(bShowCost);
+	costtext1.setVisible(bShowCost);
+	cost.setReadOnly(bReadOnly);
+	cost.setVisible(bShowCost);
+	label_cost.setVisible(bShowCost);
+	costtext2.setVisible(bShowCost);
+	label_coststat.setVisible(bShowCost);
+	coststat.setVisible(bShowCost);
+	coststat.setReadOnly(bReadOnly);
+	costtext3.setVisible(bShowCost);
+	
+	ftdesc.setReadOnly(bReadOnly);
+end
